387904a5b46d2fbd26c7a59e442223c875158a66
Christian Lahmer <christian.lahmer@sap.com>
2021-06-01 21:41:53 +0200

    changes

--- public/javascripts/function.js
+++ public/javascripts/function.js
@@ -1,3 +1,12 @@
 function showDiv(){
 document.getElementById("f2").style.visibility="visible"
 }
+
+$( document ).ready(function() {
+  $('.dropdown').each(function (key, dropdown) {
+      var $dropdown = $(dropdown);
+      $dropdown.find('.dropdown-menu a').on('click', function () {
+          $dropdown.find('button').text($(this).text()).append(' <span class="caret"></span>');
+      });
+  });
+});
